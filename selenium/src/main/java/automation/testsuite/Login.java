8a830e994560c3e50dca67f9a090c62d57619dba
Tri Võ Minh <vominhtri11112@gmail.com>
2024-10-27 22:42:17 +0700

    btvn7

--- selenium/src/main/java/automation/testsuite/Login.java
+++ selenium/src/main/java/automation/testsuite/Login.java
@@ -22,21 +22,17 @@ public void NhanVien ()
     
         Scanner scanner = new Scanner(System.in);
 
-        // Nhập số lượng nhân viên
         System.out.print("Nhập số lượng nhân viên: ");
         int soLuong = scanner.nextInt();
-        scanner.nextLine(); // Đọc dòng trống sau khi nhập số
+        scanner.nextLine();
 
-        // Khởi tạo mảng tên nhân viên
+
         String[] danhSachNhanVien = new String[soLuong];
 
-        // Nhập tên cho từng nhân viên
         for (int i = 0; i < soLuong; i++) {
             System.out.print("Nhập tên nhân viên " + (i + 1) + ": ");
             danhSachNhanVien[i] = scanner.nextLine();
         }
-
-        // In tên các nhân viên bằng vòng lặp for-each
         System.out.println("Danh sách tên nhân viên:");
         for (String ten : danhSachNhanVien) {
             System.out.println(ten);
