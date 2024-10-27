d711034f6d3dc4cb7df9c442a1345c316e70db62
Tri Võ Minh <vominhtri11112@gmail.com>
2024-10-27 21:36:27 +0700

    ( tri btvn7 )

--- selenium/src/main/java/automation/testsuite/Login.java
+++ selenium/src/main/java/automation/testsuite/Login.java
@@ -1,5 +1,7 @@
 package automation.testsuite;
 
+import java.util.Scanner;
+
 import org.testng.annotations.*;
 
 public class Login {
@@ -14,4 +16,33 @@ public void testcase1()
 {
 	System.out.print(" testcase1");
 }
+@Test
+public void NhanVien ()
+{
+    
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhập số lượng nhân viên
+        System.out.print("Nhập số lượng nhân viên: ");
+        int soLuong = scanner.nextInt();
+        scanner.nextLine(); // Đọc dòng trống sau khi nhập số
+
+        // Khởi tạo mảng tên nhân viên
+        String[] danhSachNhanVien = new String[soLuong];
+
+        // Nhập tên cho từng nhân viên
+        for (int i = 0; i < soLuong; i++) {
+            System.out.print("Nhập tên nhân viên " + (i + 1) + ": ");
+            danhSachNhanVien[i] = scanner.nextLine();
+        }
+
+        // In tên các nhân viên bằng vòng lặp for-each
+        System.out.println("Danh sách tên nhân viên:");
+        for (String ten : danhSachNhanVien) {
+            System.out.println(ten);
+        }
+
+        scanner.close();
+    }
 }
+
